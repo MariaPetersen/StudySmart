3aff08b7ebc9502bf596571fb503b8aac263d15f
alexvtl <alexandre.vital@hotmail.fr>
2023-11-09 16:19:22 +0100

    footer

---
+++ front/studysmart/src/Components/Footer/footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Footer.css";
+export default function footer() {
+  return (
+    <footer>
+      <div className="footer_section_one">
+        <div className="section_one_right">
+          <div>
+            <p>DON'T MISS</p>
+            <br />
+            <p>NEW UPDATES</p>
+          </div>
+          <div>
+            <input type="text" />
+            <button></button>
+          </div>
+        </div>
+        <div className="section_one_left">
+          <div className="section_findus">
+            <span>YOU CAN FIND US</span>
+            <ul>
+              <li>studysmart@gmail.com</li>
+              <li>01 43 49 37 80</li>
+              <li>32 Rue de la Perche16e Arr</li>
+            </ul>
+          </div>
+          <div className="section_help">
+            <span>HELP</span>
+            <ul>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+              <li>Cookies</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer_section_two">
+        When you use our services, you’re trusting us with your information. We
+        understandthis is a big responsability and work hard to protect your
+        information and put you in control.
+      </div>
+    </footer>
+  );
+}
