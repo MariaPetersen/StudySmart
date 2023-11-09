7cbc2d5e92f3333e90bf72956b7d645ce2b2d571
william <williams_wandji@yahoo.fr>
2023-11-09 16:47:47 +0100

    start page signup

--- front/studysmart/src/Pages/Signup/signup.jsx
+++ front/studysmart/src/Pages/Signup/signup.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import "./signup.css";
 export default function Signin() {
-  return <div>Sign</div>;
+  return (
+    <div className="signup">
+        <div className="left">
+          <h1>Create an account</h1>
+          <p>Create n account in order to post like other users and have a custum profile</p>
+        </div>
+    </div>
+  );
 }
