50a4bc682be32c2383f861ce9b1b24a2efe01f36
alexvtl <alexandre.vital@hotmail.fr>
2023-11-10 16:38:13 +0100

    regex input page login

--- front/studysmart/src/Pages/Login/login.jsx
+++ front/studysmart/src/Pages/Login/login.jsx
@@ -25,12 +25,28 @@ export default function login() {
 
           <form className="login_form" action="">
             <div className="input_container">
-              <p>EMAIL</p>
-              <input className="login_input" type="text" />
+              <label for="email">EMAIL</label>
+              <input
+                className="login_input"
+                type="email"
+                name="email"
+                id="email"
+                placeholder=""
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                required
+              />
             </div>
             <div className="input_container">
-              <p>PASSWORD</p>
-              <input className="login_input" type="text" />
+              <label for="password">PASSWORD</label>
+              <input
+                className="login_input"
+                type="password"
+                minlength="8"
+                name="password"
+                id="password"
+                placeholder=""
+                required
+              />
             </div>
             <div>
               <button className="login_button_form">LOG IN</button>
