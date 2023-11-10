2a74165cd4bc81f8dea784fa5df124fb60ddecf4
Maria Petersen <mau.petersen@gmail.com>
2023-11-10 18:40:18 +0100

    add publications routes

--- back/controllers/publication.js
+++ back/controllers/publication.js
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 
 async function createPublication(req, res, next) {
     try {
+        console.log("here");
         const { userId } = req.auth;
         const profile = await prisma.profile.findFirst({
             where: {
@@ -27,4 +28,47 @@ async function createPublication(req, res, next) {
     }
 }
 
-module.exports = { createPublication };
+async function getOnePublication(req, res, next) {
+    try {
+        const { id } = req.params;
+        const publicationId = parseInt(id);
+        const publication = await prisma.publication.findFirst({
+            where: {
+                id: publicationId
+            }
+        });
+        res.status(200).json(publication);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+async function getAllPublications(req, res, next) {
+    try {
+        const offset = req.query?.offset;
+        const limit = req.query?.limit;
+        const publications = await prisma.publication.findMany({
+            skip: offset ? parseInt(offset) : 0,
+            take: limit ? parseInt(limit) : 100
+        });
+        res.status(200).json(publications);
+    } catch {
+        res.status(500).json(error);
+    }
+}
+
+async function updatePublication(req, res, next) {
+    try {
+        // const offset = req.query?.offset;
+        // const limit = req.query?.limit;
+        const publications = await prisma.publication.findMany({
+            skip: offset ? offset : null,
+            take: limit ? limit : null
+        });
+        res.status(201).json(publications);
+    } catch {
+        res.status(500).json(error);
+    }
+}
+
+module.exports = { createPublication, getOnePublication, getAllPublications, updatePublication };
