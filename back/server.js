e462fd44422b1eeff8ea8415ce350d66eedb2149
Joachim Renevier <joachim.renevier@gmail.com>
2023-11-16 21:31:31 +0100

    wip

--- back/server.js
+++ back/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv');
@@ -10,17 +9,4 @@ const server = http.createServer(app);
 
 server.listen(process.env.BACK_PORT, () => {
   console.log(`Server is running on port ${process.env.BACK_PORT}`);
-=======
-const http = require('http');
-const app = require('./app');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-app.set('port', process.env.BACK_PORT);
-const server = http.createServer(app);
-
-server.listen(process.env.BACK_PORT, () => {
-  console.log(`Server is running on port ${process.env.BACK_PORT}`);
->>>>>>> 20bc8b41fa2f0b59722ac124a08250daff57994c
 });
