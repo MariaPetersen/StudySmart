b3d37efce9606b97fe7c9eab490d77edbe93f672
william <williams_wandji@yahoo.fr>
2023-11-25 14:02:03 +0100

    page publication end

--- front/studysmart/src/Router/App.js
+++ front/studysmart/src/Router/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "../Pages/Signup/signup";
 import Login from "../Pages/Login/login";
+import Publication from "../Pages/Publication/publication";
 import Nav from "../Components/Nav/nav";
 import Footer from "../Components/Footer/footer";
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/publication" element={<Publication/>} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 }
