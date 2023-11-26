3ec78df0ff02b3a99881c1327818115c30c486c9
Maria Petersen <mau.petersen@gmail.com>
2023-11-26 19:01:27 +0100

    clean up

--- back/routes/images.js
+++ back/routes/images.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const auth = require("./../middleware/auth");
 const imagesCtrl = require("./../controllers/images");
 
-// router.get('/:id', auth, imagesCtrl.getOneImage);
-// router.put('/:id', auth, iamgesCtrl.updateImage);
 router.delete('/:id', auth, imagesCtrl.deleteImage);
 router.post('/', auth, imagesCtrl.uploadImage);
 
