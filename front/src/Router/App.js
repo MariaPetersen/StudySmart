45adcdec20a7794ebd03827a6a5952a476168cf3
alexvtl <alexandre.vital@hotmail.fr>
2023-11-28 14:59:58 +0100

    context user + page home

--- front/src/Router/App.js
+++ front/src/Router/App.js
@@ -1,18 +1,23 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "../Pages/Signup/signup";
-import Login from "../Pages/Login/login";
-import Nav from "../Components/Nav/nav";
-import Footer from "../Components/Footer/footer";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from '../Pages/Signup/signup';
+import Login from '../Pages/Login/login';
+import Nav from '../Components/Nav/nav';
+import Footer from '../Components/Footer/footer';
+import Home from '../Pages/Home/home';
+import { UserContextProvider } from '../Context/Usercontext';
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <Footer />
+      <UserContextProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer />
+      </UserContextProvider>
     </BrowserRouter>
   );
 }
