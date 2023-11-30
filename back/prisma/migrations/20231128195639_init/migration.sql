c8a8e210983469468cf37bf34dbd12dcf3f8c5ea
Joachim Renevier <joachim.renevier@gmail.com>
2023-11-30 13:31:37 +0100

    regbase

---
+++ back/prisma/migrations/20231128195639_init/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE `Comments` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `publication_id` INTEGER NOT NULL,
+    `text` TEXT NOT NULL,
+    `reaction` TEXT NOT NULL,
+    `reponse` TEXT NOT NULL,
+    `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `description` TEXT NOT NULL,
+
+    UNIQUE INDEX `Profile_user_id_key`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Publication` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `profile_id` INTEGER NOT NULL,
+    `title` CHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `text` TEXT NOT NULL,
+    `link` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `lastname` TEXT NOT NULL,
+    `firstname` TEXT NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` TEXT NOT NULL,
+
+    UNIQUE INDEX `email`(`email`),
+    UNIQUE INDEX `username`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- back/server.js
+++ back/server.js
@@ -2,9 +2,10 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv');
 
-dotenv.config;
+dotenv.config();
 
-app.set('port', process.env.PORT);
+app.set('port', process.env.BACK_PORT);
 const server = http.createServer(app);
 
-server.listen(process.env.PORT);
+server.listen(process.env.BACK_PORT);
+
