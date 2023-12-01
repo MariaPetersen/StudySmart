ffb29526c1292565fbca781028746c014566c294
Maria Petersen <mau.petersen@gmail.com>
2023-12-01 11:05:29 +0100

    add profile picutre to database

--- back/controllers/images.js
+++ back/controllers/images.js
@@ -10,7 +10,7 @@ async function uploadImage(req, res, next) {
             return res.status(400).json({ error: "No File Selected" });
         }
 
-        const result = await cloudinary.uploader.upload(file.data, {
+        const result = await cloudinary.uploader.upload(file.tempFilePath, {
             public_id: `${Date.now()}`,
             resource_type: "auto",
             folder: "profileImages"
